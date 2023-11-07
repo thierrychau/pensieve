12ec4e74d8ebc46afc310afb50877cd1413c0875
Thierry Chau <thierry.chau@gmail.com>
2023-11-07 18:28:34 +0000

    changed user email to use citext

--- db/migrate/20231107173745_devise_create_users.rb
+++ db/migrate/20231107173745_devise_create_users.rb
@@ -3,8 +3,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      enable_extension("citext")
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.citext :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -14,7 +15,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Trackable
+      ``## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
       # t.datetime :last_sign_in_at
@@ -30,7 +31,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+      # t.datetime :locked_at``
 
 
       t.timestamps null: false

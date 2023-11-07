9c24f396a67026affe05202a895e739d84c6bb86
Thierry Chau <thierry.chau@gmail.com>
2023-11-07 20:59:24 +0000

    generated scaffold memory

---
+++ app/views/memories/_memory.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! memory, :id, :date, :location, :latitude, :longitude, :description, :author_id, :created_at, :updated_at
+json.url memory_url(memory, format: :json)
---
+++ db/migrate/20231107205902_create_memories.rb
@@ -0,0 +1,14 @@
+class CreateMemories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :memories do |t|
+      t.date :date
+      t.string :location
+      t.decimal :latitude
+      t.decimal :longitude
+      t.text :description
+      t.references :author, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

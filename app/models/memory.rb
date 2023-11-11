7c36d0e4ef1fabbfc1ef0b868db943f0bf5e3aa5
Thierry Chau <thierry.chau@gmail.com>
2023-11-11 03:08:27 +0000

    implemented fetch coordinates before saving memory

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -23,6 +23,8 @@
 class Memory < ApplicationRecord
   validates :date, :presence => true
   validates :author_id, :presence => true
+
+  before_save :memory_coordinates
   
   belongs_to :author, class_name: "User"
   has_many :people_memories
@@ -34,5 +36,15 @@ class Memory < ApplicationRecord
     [
       "description"
     ]
+  end
+
+  def memory_coordinates
+    geocoding_service = GeocodingService.new(self.location)
+    coordinates = geocoding_service.call
+
+    if coordinates
+      self.latitude = coordinates.fetch(:latitude)
+      self.longitude = coordinates.fetch(:longitude)
+    end
   end
 end

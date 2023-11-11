0c1e9f771b9988979265f727b5074151d0828f73
Thierry Chau <thierry.chau@gmail.com>
2023-11-11 01:21:14 +0000

    created location controller and geocoding services

---
+++ app/services/geocoding_service.rb
@@ -0,0 +1,26 @@
+require "http"
+require "json"
+
+class GeocodingService
+  def initialize(location)
+    @location = location
+  end
+
+  def call
+    raw_response = HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@location}&key=#{GMAPS_API_KEY}")
+    parsed_response = JSON.parse(raw_response)
+
+    if parsed_response["status"] == "OK"
+      results = parsed_response["results"].at(0)
+      geometry = results.fetch("geometry")
+      location = geometry.fetch("location")
+      
+      {
+        :latitude => location.fetch("lat"),
+        :longitude => location.fetch("lng")
+      }
+    else
+      nil
+    end
+  end
+end

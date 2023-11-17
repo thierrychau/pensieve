7f1b26ea81b51cf2e0eb5d8cf436af963ef6bba5
Thierry Chau <thierry.chau@gmail.com>
2023-11-17 17:17:14 +0000

    cleaner users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 
 class UsersController < ApplicationController
-  before_action :set_people, only: %i[ dashboard ]
+  before_action :set_people, only: %i[ dashboard search ]
+  before_action :set_memories, only: %i[ dashboard search ]
 
   def dashboard
-    @q = Memory.ransack(params[:q])
-    # @q.sorts = ['date desc', 'location asc'] if @q.sorts.empty?
+    @q = @user_memories.ransack(params[:q])
     @q.sorts = ['date desc'] if @q.sorts.empty?
     @memories = @q.result
-    @memory = Memory.new
-    @memory.build_address
-    @person = Person.new
+    @memory = Memory.new # for nested form
+    @person = Person.new # for nested form
+    @memory.build_address # for nested form
   end
 
   private
-    def set_people
-      @people = Person.all
+    def set_memories
+      @user_memories = current_user.memories.all
     end
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password)
+    
+    def set_people
+      @people = current_user.people.all
     end
 end

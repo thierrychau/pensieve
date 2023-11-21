0c572feec3d4a3a710d508b4416a612bb8e719fc
Thierry Chau <thierry.chau@gmail.com>
2023-11-21 22:49:11 +0000

    setting up pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pundit::Authorization
 
-  def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar_url])
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+  private
+    def configure_permitted_parameters
+      # devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar_url])
 
-    # devise_parameter_sanitizer.permit(:account_update, :keys => [:avatar_url])
-  end
+      # devise_parameter_sanitizer.permit(:account_update, :keys => [:avatar_url])
+    end
 
-  def after_sign_in_path_for(resource)
-    root_path
-  end
+    def after_sign_in_path_for(resource)
+      root_path
+    end
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_back(fallback_location: root_path)
+    end
 end
--- app/policies/memory_policy.rb
+++ app/policies/memory_policy.rb
@@ -1,3 +1,32 @@
 class MemoryPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
 
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
+  end
 end

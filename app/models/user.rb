8ccdfdeed38a3d03fa4d6e7b35b2c1e10360f8e3
Thierry Chau <thierry.chau@gmail.com>
2023-11-28 11:59:42 -0600

    restored registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,8 +31,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, #:registerable,
-         :recoverable, :rememberable, :validatable#, :confirmable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
 
   validate :password_complexity, :if => :password
 

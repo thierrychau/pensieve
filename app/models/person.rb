1a7fa35e6e6b7ced13b098b6895fc74d08360b7d
Thierry Chau <thierry.chau@gmail.com>
2023-11-29 09:30:41 -0600

    added dob to people

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,12 +2,13 @@
 #
 # Table name: people
 #
-#  id         :bigint           not null, primary key
-#  first_name :string           not null
-#  last_name  :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint
+#  id            :bigint           not null, primary key
+#  date_of_birth :date
+#  first_name    :string           not null
+#  last_name     :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :bigint
 #
 # Indexes
 #

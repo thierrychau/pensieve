8d48b24b312ba3b952a8c964c601684741772be6
Thierry Chau <thierry.chau@gmail.com>
2023-12-01 16:12:19 -0600

    fixing minor bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
   after_action :verify_authorized, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
 
@@ -10,12 +9,6 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   
   private
-    def configure_permitted_parameters
-      # devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar_url])
-
-      # devise_parameter_sanitizer.permit(:account_update, :keys => [:avatar_url])
-    end
-
     def after_sign_in_path_for(resource)
       root_path
     end

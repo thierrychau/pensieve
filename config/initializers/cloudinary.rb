90f73e42059c40fdb59a1171f2c7322ed5a61ee5
Thierry Chau <thierry.chau@gmail.com>
2023-12-05 16:33:07 -0600

    using active storage instead of carrierwave

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -3,4 +3,8 @@ Cloudinary.config do |config|
   config.api_key = ENV["CLOUDINARY_API_KEY"]
   config.api_secret = ENV["CLOUDINARY_API_SECRET"]
   config.cdn_subdomain = true
+  config.secure = true
 end
+
+Cloudinary.config_from_url(ENV['CLOUDINARY_URL'])
+pp "Initializer: cloudinary #{Cloudinary.config.cloud_name}"
